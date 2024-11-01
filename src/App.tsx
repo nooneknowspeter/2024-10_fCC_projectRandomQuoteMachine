3a6437a7d7c3fbff9d28de9224363ace224617b3
peter <jerelepeter06@gmail.com>
2024-11-01 09:40:21 -0400

    prototyping in code using figma wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,31 @@ import "/node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
-    <div>
-      does this work??? === ??? :=
-      <button type="button" className="btn btn-primary">
-        wtf<span className="bi bi-0-circle"></span>
-      </button>
-    </div>
+    <>
+      <div
+        className="container text-secondary position-absolute top-50 start-50 translate-middle text-wrap mx-auto p-2"
+        style={{ maxWidth: 800, minWidth: 250 }}
+      >
+        <p className="text-center text-wrap fw-semibold ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p className="text-end fw-medium">- author</p>
+        <div className="d-flex justify-content-between">
+          <button type="button" className="btn btn-primary col-sm-1">
+            <i className="bi bi-twitter-x"></i>
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary col-sm-2 fw-semibold"
+          >
+            New Quote
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
