9c7cf7f699aed905a7ab4d31fe66e283247c1d11
peter <jerelepeter06@gmail.com>
2024-11-01 09:56:53 -0400

    passed all tests with prototype time for api fetching and requests animations and hopefully 3D cg

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,37 @@ import "/node_modules/bootstrap-icons/font/bootstrap-icons.css";
 function App() {
   return (
     <>
-      <div className="container-fluid bg-primary position-absolute top-0 bottom-0 start-0 end-0">
+      <div className="container-fluid bg-primary position-absolute top-0 bottom-0 start-0 end-0 background">
         ff
       </div>
       <div
         className="container text-secondary position-absolute top-50 start-50 translate-middle text-wrap mx-auto p-2"
         style={{ maxWidth: 800, minWidth: 250 }}
+        id="quote-box"
       >
-        <p className="text-center text-wrap fw-semibold ">
+        <p className="text-center text-wrap fw-semibold " id="text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
-        <p className="text-end fw-medium">- author</p>
+        <p className="text-end fw-medium" id="author">
+          - author
+        </p>
         <div className="d-flex justify-content-between">
-          <button type="button" className="btn btn-primary col-sm-1">
+          <a
+            type="button"
+            className="btn btn-primary col-sm-1"
+            id="tweet-quote"
+            target="_blank"
+            href="twitter.com/intent/tweet"
+          >
             <i className="bi bi-twitter-x"></i>
-          </button>
+          </a>
           <button
             type="button"
             className="btn btn-primary col-sm-2 fw-semibold"
+            id="new-quote"
           >
             New Quote
           </button>
