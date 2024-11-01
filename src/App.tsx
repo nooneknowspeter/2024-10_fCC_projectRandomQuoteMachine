afadac3d620fecd0626da13f6cf25a2291722149
peter <jerelepeter06@gmail.com>
2024-11-01 08:23:34 -0400

    added a components folder for individual components

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,9 @@ import "/src/sass/main.css";
 
 function App() {
   return (
-    <>
-      <button type="button" class="btn btn-primary">
-        Primary
-      </button>
-      <button type="button" class="btn btn-secondary">
-        Secondary
-      </button>
-      <button type="button" class="btn btn-success">
-        Success
-      </button>
-      <button type="button" class="btn btn-danger">
-        Danger
-      </button>
-      <button type="button" class="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" class="btn btn-info">
-        Info
-      </button>
-      <button type="button" class="btn btn-light">
-        Light
-      </button>
-      <button type="button" class="btn btn-dark">
-        Dark
-      </button>
-
-      <button type="button" class="btn btn-link">
-        Link
-      </button>
-    </>
+    <div className="container text-center align-items-center">
+      does this work??? === ??? :=
+    </div>
   );
 }
 
