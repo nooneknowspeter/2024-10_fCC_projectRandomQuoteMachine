6cfabb39676f48892f4d1c5b412a541f69ab49dc
peter <jerelepeter06@gmail.com>
2024-11-02 05:06:36 -0400

    added something to the github repo button

--- src/components/GitHubRepoButton.tsx
+++ src/components/GitHubRepoButton.tsx
@@ -1,5 +1,31 @@
 import "/src/sass/main.css";
 import "/node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import gsap from "gsap/all";
+
+const MouseOn = (event: object) => {
+  if (event) {
+    // console.log(true);
+    // gsap anim
+    gsap.to("#github-repo-btn", {
+      duration: 0.5,
+      ease: "power2.out",
+      color: "white",
+    });
+  }
+};
+
+const MouseOff = (event: object) => {
+  if (event) {
+    // console.log(false);
+
+    // gsap anim
+    gsap.to("#github-repo-btn", {
+      duration: 0.5,
+      ease: "power2.out",
+      color: "#0c0c0c",
+    });
+  }
+};
 
 const GitHubRepoButton = () => {
   return (
@@ -9,8 +35,10 @@ const GitHubRepoButton = () => {
         href="https://github.com/nooneknowspeter/2024-10_fCC_projectRandomQuoteMachine"
         type="button"
         className="col-sm-1"
+        onMouseMove={MouseOn}
+        onMouseOut={MouseOff}
       >
-        <i className="bi bi-github"></i>
+        <i className="bi bi-github " id="github-repo-btn"></i>
       </a>
     </span>
   );
