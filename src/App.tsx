aeac9bd85f7c10e8e620d76120806dff30452742
peter <jerelepeter06@gmail.com>
2024-11-02 02:33:40 -0400

    got animations to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "/src/sass/main.css";
 import "/node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import Background from "./components/Background";
@@ -6,6 +6,7 @@ import NewQuoteButton from "./components/NewQuoteButton";
 import QuoteAssembly from "./components/QuoteAssembly";
 import ShareButton from "./components/ShareButton";
 import GitHubRepoButton from "./components/GitHubRepoButton";
+import { gsap } from "gsap";
 
 function App() {
   // API links
@@ -17,6 +18,9 @@ function App() {
     quote: string;
     author: string;
   } | null>(null);
+  const [initialText, setInitialText] = useState(true);
+
+  const quoteRef = useRef<HTMLDivElement>(null);
 
   // how data looks from json
   //   [{ quote: "quote", author: "author" },...]
@@ -26,7 +30,10 @@ function App() {
       const res = await fetch(quotesjson);
       const data = await res.json();
       setQuotes(data);
-      setCurrentQuote(data[0]);
+      setTimeout(() => {
+        setInitialText(false); // Switch to quote text after initial display
+        setCurrentQuote(data[0]);
+      }, 2000); // Delay before switching to the first quote
     } catch (error) {
       console.log(error);
     }
@@ -39,7 +46,17 @@ function App() {
   const changeQuote = () => {
     if (quotes.length > 0) {
       const randomIndex = Math.floor(Math.random() * quotes.length);
-      setCurrentQuote(quotes[randomIndex]); // Set a new random quote
+      const newQuote = quotes[randomIndex];
+
+      // GSAP fade-out, update quote, then fade-in
+      gsap.to(quoteRef.current, {
+        opacity: 0,
+        duration: 1,
+        onComplete: () => {
+          setCurrentQuote(newQuote);
+          gsap.to(quoteRef.current, { opacity: 1, duration: 1 });
+        },
+      });
     }
   };
 
@@ -48,19 +65,31 @@ function App() {
       <Background />
       <div
         className="container text-secondary position-absolute top-50 start-50 translate-middle text-wrap mx-auto p-2"
-        style={{ maxWidth: 800, minWidth: 250 }}
+        style={{ maxWidth: 800, minWidth: 250, maxHeight: 150, minHeight: 50 }}
         id="quote-box"
       >
-        {currentQuote ? (
-          <QuoteAssembly
-            quoteText={currentQuote.quote}
-            quoteAuthor={currentQuote.author}
-          />
-        ) : (
-          <p>Loading...</p>
-        )}
+        <div ref={quoteRef} style={{ opacity: 1 }}>
+          {initialText ? (
+            <p className="text-center text-wrap fw-semibold " id="text">
+              Hello World
+            </p>
+          ) : currentQuote ? (
+            <QuoteAssembly
+              quoteText={currentQuote.quote}
+              quoteAuthor={currentQuote.author}
+            />
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
         <div className="d-flex justify-content-between">
-          <ShareButton />
+          <ShareButton
+            href={
+              currentQuote
+                ? `"` + currentQuote.quote + `" ` + " - " + currentQuote.author
+                : ""
+            }
+          />
           <NewQuoteButton onClick={changeQuote} />
         </div>
         <GitHubRepoButton />
--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,14 +1,17 @@
 import "/src/sass/main.css";
 import "/node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
-const ShareButton = () => {
+const ShareButton = (props: { href: string }) => {
   return (
     <a
       type="button"
       className="btn btn-primary col-sm-1 btn-sm"
       id="tweet-quote"
       target="_blank"
-      href={`https://www.twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=`}
+      href={
+        "https://www.twitter.com/intent/tweet?hashtags=quotes&hashtags=quotesgen&text=" +
+        props.href
+      }
     >
       <i className="bi bi-twitter-x"></i>
     </a>
